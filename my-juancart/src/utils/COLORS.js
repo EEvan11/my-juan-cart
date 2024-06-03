62372f10d98a3221b22bc788ac73f5a13352ee7f
bryanken00 <bryanken01230@gmail.com>
2024-06-03 21:13:37 +0800

    Test

---
+++ my-juancart/src/utils/COLORS.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: "#3498db",
+  secondary: "#2980b9",
+  danger: "#e74c3c",
+  success: "#2ecc71",
+  warning: "#f1c40f",
+  info: "#9b59b6",
+  light: "#ecf0f1",
+  dark: "#34495e",
+  white: "#fff",
+  black: "#000",
+};
