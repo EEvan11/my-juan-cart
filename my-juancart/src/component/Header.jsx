8bfd620057f303a3753bdbfed808cef2b6f93178
EMcodes <110814562+EEvan11@users.noreply.github.com>
2024-06-03 21:30:55 +0800

    it

--- my-juancart/src/component/Header.jsx
+++ my-juancart/src/component/Header.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Nav, Content, Footer } = Layout;
+
+const menuLabels = [
+  'Home',
+  'About',
+  'Services',
+  'Contact'
+];
+
+
+const items = menuLabels.map((label, index) => ({
+  key: index + 1,
+  label: label,
+}));
 
 const Header = () => {
-  return <div>Header</div>;
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Nav
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Nav>
+    </Layout>
+  );
 };
 
 export default Header;
+
+
+
+
